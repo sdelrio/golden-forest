2c93d4977d074e387ff9c8bd61492df8d8ecf106
gh-actions <sdelrio@users.noreply.github.com>
2022-01-29 22:35:01 +0000

    Deploy website - based on c78f1e0cb262c6ee293f9deecf19f52b5abdc3b7

---
+++ assets/js/050cf89e.911c55a7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgolden_forest_website=self.webpackChunkgolden_forest_website||[]).push([[6788],{34252:function(s){s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/k-8-s","name":"k8s","count":1,"permalink":"/blog/tags/k-8-s"}')}}]);
