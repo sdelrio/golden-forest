7f4f87769298968e4d80f9c05085738750399e0d
gh-actions <sdelrio@users.noreply.github.com>
2024-01-27 19:51:33 +0000

    Deploy website - based on f68c27c26e0e5cfe32866738c013cfc3511aed7b

---
+++ assets/js/d106a616.716c5c6f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgolden_forest_website=self.webpackChunkgolden_forest_website||[]).push([[6060],{58166:e=>{e.exports=JSON.parse('{"label":"xfce4","permalink":"/docs/tags/xfce-4","allTagsPath":"/docs/tags","count":1,"items":[{"id":"OS/GNU-Linux/wm","title":"Window Managers","description":"i3","permalink":"/docs/OS/GNU-Linux/wm"}]}')}}]);
