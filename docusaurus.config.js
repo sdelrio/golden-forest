221938c86b7e9ec6750469918f75bee222e873ac
sdelrio <sdelrio@users.noreply.github.com>
2025-05-01 12:23:38 +0200

    docusaurus: fix config copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -202,7 +202,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} The Golden Forest project. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
