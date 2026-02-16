7cd87544e3adbb46a5da10e43e50b1114a481c42
sdelrio <sdelrio@users.noreply.github.com>
2026-02-16 19:25:25 +0100

    feat(dnd): refactor content to item list with toc css

---
+++ src/components/IndexList.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import {
+    useCurrentSidebarSiblings,
+    filterDocCardListItems,
+} from '@docusaurus/plugin-content-docs/client';
+
+function DocumentIndex({ item }) {
+    if (!item.href) {
+        return null;
+    }
+
+    return (
+        <li>
+            <Link
+                to={item.href}
+                className="table-of-contents__link">
+                {item.label}
+            </Link>
+        </li>
+    );
+}
+
+export default function IndexList() {
+    const items = useCurrentSidebarSiblings();
+    const filteredItems = filterDocCardListItems(items);
+
+    if (!filteredItems || filteredItems.length === 0) {
+        return null;
+    }
+
+    return (
+        <ul className="table-of-contents">
+            {filteredItems.map((item, index) => (
+                <DocumentIndex
+                    key={index}
+                    item={item}
+                />
+            ))}
+        </ul>
+    );
+}
