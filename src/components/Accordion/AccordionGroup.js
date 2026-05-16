c6844743c5cd5f790ccce1fd45a56e31373eaf52
sdelrio <sdelrio@users.noreply.github.com>
2026-05-16 20:25:59 +0200

    feat: component accordion css and state

--- src/components/Accordion/AccordionGroup.js
+++ src/components/Accordion/AccordionGroup.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import clsx from 'clsx';
+import styles from './Accordion.module.css';
 
 export default function AccordionGroup({ children }) {
   return (
-    <div className="accordion-group">
+    <div className={clsx('accordion-group', styles.accordionGroup)}>
       {children}
     </div>
   );
