3c67af5d154b5d1ef357cb67fd027d2526e9b15f
sdelrio <sdelrio@users.noreply.github.com>
2026-07-19 18:34:40 +0000

    Deploy preview for PR 182 🛫

---
+++ pr-preview/pr-182/assets/js/11886.e2fa8b1f.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunkgolden_forest_website=self.webpackChunkgolden_forest_website||[]).push([["11886"],{80893(e,t,s){s.d(t,{diagram:()=>a});var r=s(52703);s(64918),s(96755),s(59337),s(81818),s(6923),s(72389),s(1778),s(75999),s(23665),s(91865),s(3868),s(97223),s(17245),s(22595),s(56149),s(31293);var l=(0,s(86827).K)(e=>`${(0,r.tM)(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),a=(0,r.ur)({defaultLayout:"swimlane",styles:l})}}]);
---
+++ pr-preview/pr-182/assets/js/4542e9b0.f2f79c30.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgolden_forest_website=self.webpackChunkgolden_forest_website||[]).push([["51065"],{69680(e){e.exports=JSON.parse('{"metadata":{"permalink":"/pr-preview/pr-182/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
