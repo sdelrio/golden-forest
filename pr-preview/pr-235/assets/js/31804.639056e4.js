effea82ac340f6234a188f96157b81239ec2dd84
sdelrio <sdelrio@users.noreply.github.com>
2026-08-26 10:03:52 +0000

    Deploy preview for PR 235 🛫

---
+++ pr-preview/pr-235/assets/js/31804.639056e4.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunkgolden_forest_website=self.webpackChunkgolden_forest_website||[]).push([["31804"],{52535(e,t,s){s.d(t,{diagram:()=>a});var r=s(49352);s(64918),s(96755),s(78257),s(81818),s(21570),s(91730),s(9290),s(9636),s(14782),s(91865),s(6129),s(70318),s(64968),s(43723),s(99011),s(31293);var l=(0,s(86827).K)(e=>`${(0,r.tM)(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),a=(0,r.ur)({defaultLayout:"swimlane",styles:l})}}]);
